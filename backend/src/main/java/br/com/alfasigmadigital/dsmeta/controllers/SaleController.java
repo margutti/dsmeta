5bb0be2c4e34a7861c49ccb631e787da1526af07
Sergio Roberto Margutti <margutti@softmidia.com.br>
2022-07-13 13:12:20 -0300

    API test

---
+++ backend/src/main/java/br/com/alfasigmadigital/dsmeta/controllers/SaleController.java
@@ -0,0 +1,25 @@
+package br.com.alfasigmadigital.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alfasigmadigital.dsmeta.entities.Sale;
+import br.com.alfasigmadigital.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public List<Sale> findSales() {
+		return service.findSales();
+	}
+
+}
---
+++ backend/src/main/java/br/com/alfasigmadigital/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package br.com.alfasigmadigital.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alfasigmadigital.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
